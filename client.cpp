39dbae38a3f3a6f092f4a4530e8a18bf8390079c
xxr3376@gmail.com <xxr3376@gmail.com>
2013-04-16 22:49:47 +0800

    upload version

--- client.cpp
+++ client.cpp
@@ -1,25 +1,59 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-#include <stdlib.h>
+#include <cstdlib>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <ctime>
 #include <netinet/in.h>
+using namespace std;
 
 #define MAX_RETRY 10
 #define DEST_PORT 3000
 #define DSET_IP_ADDRESS  "127.0.0.1"
 
+void splitData(char* req, char** data, int* no){
+	*data = req + 16;
+	int d = 0;
+	for (int i = 0; i < 16; i++){
+		d *= 10;
+		d += (req[i] - '0');
+	}
+	*no = d;
+}
+
+
+int getID(){
+	return rand()*rand()*rand();
+}
+
+char send_buf[548] = {};
+char recv_buf[1000];
+
+int CurID;
+
 int main(int argc, const char *argv[])
 {
+	// input check
+	srand(time(NULL));
+	CurID = getID();
+	if (argc == 2){
+		if (strlen(argv[1]) > 500){
+			printf("input is too long, please input a easier one\n");
+			exit(-1);
+		}
+		sprintf(send_buf, "%016u%s", CurID, argv[1]);
+	}
+	else{
+		sprintf(send_buf, "%016u1+1", CurID);
+	}
+	//INIT
 	int sock_fd;/*套接字文件描述符*/
 	int send_num;
 	int recv_num;
 	int dest_len;
-	char send_buf[1000] = {"1+1"};
-	char recv_buf[1000];
 	struct sockaddr_in addr_serv;/*服务端地址，客户端地址*/
 
 	sock_fd = socket(AF_INET,SOCK_DGRAM,0);//创建套接子
@@ -28,26 +62,60 @@ int main(int argc, const char *argv[])
 	addr_serv.sin_family = AF_INET;
 	addr_serv.sin_addr.s_addr = inet_addr(DSET_IP_ADDRESS);
 	addr_serv.sin_port = htons(DEST_PORT);
-
 	dest_len = sizeof(struct sockaddr_in);
-	printf("begin send:\n");
-	if (argc == 2){
-		strcpy(send_buf, argv[1]);
+	send_num = -1;
+	struct timeval tv;
+	tv.tv_sec = 4;
+	tv.tv_usec = 0;
+	setsockopt(sock_fd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+
+	//Send
+	int sendRetryCounter = 0;
+	int waitRetryCounter = 0;
+	while (true){
+		send_num = sendto(sock_fd,send_buf,sizeof(send_buf),0,(struct sockaddr *)&addr_serv,dest_len);
+		if(send_num < 0){
+			fprintf(stderr, "Socket Fail");
+			if (++sendRetryCounter > MAX_RETRY){
+				fprintf(stderr, ", Already try 10 times, please check network\n");
+				exit(-1);
+			}
+			fprintf(stderr, ", Retry to send\n");
+			continue;
+		}
+		recv_num = recvfrom(sock_fd,recv_buf,sizeof(recv_buf),0,(struct sockaddr *)&addr_serv,(socklen_t *)&dest_len);
+		if (recv_num < 0 ){
+			if (errno == EWOULDBLOCK) {
+				fprintf(stderr, "Server timeout");
+				if (++waitRetryCounter > MAX_RETRY){
+					fprintf(stderr, ", Already try 10 times, please check network or complex of your input");
+					exit(-1);
+				}
+				fprintf(stderr,", Retry to send same request\n");
+				continue;
+			} else{
+				fprintf(stderr, "Unknown Error\n");
+				exit(-1);
+			}
+		}
+		// Success
+		
+		recv_buf[recv_num]='\0';
+		char* answer;
+		int recvNo;
+		splitData(recv_buf, &answer, &recvNo);
+		if (CurID != recvNo){
+			fprintf(stderr, "Wrong ID");
+			if (++waitRetryCounter > MAX_RETRY){
+				fprintf(stderr, ", Server responses illegally, Failed\n");
+				exit(-1);
+			}
+			continue;
+		}
+		printf("the answer is :%s\n",answer);
+		break;
 	}
-	send_num = sendto(sock_fd,send_buf,sizeof(send_buf),0,(struct sockaddr *)&addr_serv,dest_len);
-	if(send_num < 0){
-		perror("sendto");
-		exit(1);
-	} else{
-		printf("send sucessful:%s\n",send_buf);
-	}
-	recv_num = recvfrom(sock_fd,recv_buf,sizeof(recv_buf),0,(struct sockaddr *)&addr_serv,(socklen_t *)&dest_len);
-	if(recv_num <0 ){
-		perror("recv_from");
-		exit(1);
-	}
-	recv_buf[recv_num]='\0';
-	printf("the answer:%s\n",recv_buf);
 	close(sock_fd);
 	return 0;
 }
+
--- server.cpp
+++ server.cpp
@@ -8,10 +8,11 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
-#include <vector>
 #include <string>
+#include <map>
 using namespace std;
 #define MAX_RETRY 3
+#define SERV_PORT 3000
 
 struct Request{
 	int sock_fd;
@@ -20,8 +21,19 @@ struct Request{
 	string data;
 };
 
-#define SERV_PORT 3000
+void splitData(const char* req, const char** data, int* no){
+	*data = req + 16;
+	int d = 0;
+	for (int i = 0; i < 16; i++){
+		d *= 10;
+		d += (req[i] - '0');
+	}
+	*no = d;
+}
+map<int, string> dataStorage;
+map<int, int> requestNumber;
 void* replyClient(void* args);
+
 int main(int argc, const char *argv[])
 {
 	int sock_fd;   //套接子描述符号
@@ -33,10 +45,8 @@ int main(int argc, const char *argv[])
 	struct sockaddr_in addr_client;//服务器和客户端地址
 	sock_fd = socket(AF_INET,SOCK_DGRAM,0);
 	if(sock_fd < 0){
-		perror("socket");
+		perror("socket error");
 		exit(1);
-	} else{
-		printf("sock sucessful\n");
 	}
 	//初始化服务器断地址
 	memset(&addr_serv,0,sizeof(struct sockaddr_in));
@@ -45,17 +55,15 @@ int main(int argc, const char *argv[])
 	addr_serv.sin_addr.s_addr = htonl(INADDR_ANY);//任意本地址
 	Request* request;
 	client_len = sizeof(struct sockaddr_in);
-	/*绑定套接子*/
 	if(bind(sock_fd,(struct sockaddr *)&addr_serv,sizeof(struct sockaddr_in))<0 ){
-		perror("bind");
+		perror("bind error");
 		exit(1);
-	} else{
-		printf("bind sucess\n");
 	}
+	printf("server is ready:\n");
+	pthread_attr_t attr;
 	while (1){
-		printf("server is ready:\n");
 		recv_num = recvfrom(sock_fd,recv_buf,sizeof(recv_buf),0,(struct sockaddr *)&addr_client,(socklen_t *)&client_len);
-		printf("rece\n");
+		printf("receive a request!!\n");
 		if(recv_num >=  0){
 			recv_buf[recv_num]='\0';
 			request = new Request();
@@ -63,21 +71,14 @@ int main(int argc, const char *argv[])
 			request->addr_client = addr_client;
 			request->client_len = client_len;
 			request->data = recv_buf;
-			replyClient(request);
 			pthread_t thread; 
 
-			pthread_attr_t attr;
 			pthread_attr_init(&attr);
-			//pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM);
-			///*  设置线程为分离属性*/ 
+			//设置线程为分离属性
 			pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
-			int t;	
-			t = pthread_create(&thread,&attr,&replyClient,(void *)request); 
-			printf("%d\n", t);
-			printf("pleaseTT");
+			pthread_create(&thread,&attr,&replyClient,(void *)request); 
 			pthread_attr_destroy (&attr);
 		}
-		printf("loop end\n");
 	}
 	pthread_exit(NULL);
 	close(sock_fd);
@@ -86,15 +87,37 @@ int main(int argc, const char *argv[])
 void *replyClient(void* args){
 	Request* request = (Request*) args;
 	int retryCounter = 0;
-	char outputBuffer[1000];
+	char bcBuf[1000];
+	char sendBuf[1000];
 	char temp[2000];
-	sprintf(temp, "echo \"%s\" | bc", request->data.c_str());
-	FILE *fp = popen(temp, "r");
-	fscanf(fp, "%s", outputBuffer);
-	int send_num = -1;
-	while (send_num < 0 && retryCounter < MAX_RETRY){
-		send_num = sendto(request->sock_fd,outputBuffer,strlen(outputBuffer),0,(struct sockaddr *)&(request->addr_client), request->client_len);
-		retryCounter++;
+	while(true){
+		if (request->data.length() > 548){
+			break;
+		}
+		int ID;
+		const char* p;
+		splitData(request->data.c_str(), &p, &ID);
+		if (dataStorage.find(ID) != dataStorage.end()){
+			printf("from cache!!!\n");
+			string out = dataStorage[ID];
+			sendto(request->sock_fd,out.c_str(),out.length(),0,(struct sockaddr *)&(request->addr_client), request->client_len);
+			break;
+		}
+		if (requestNumber.find(ID) != requestNumber.end()){
+			printf("request is running, abandon");
+			break;
+		}
+		requestNumber[ID] = 1;
+
+		//Start to calculate
+		sprintf(temp, "echo \"%s\" | bc", p);
+		FILE *fp = popen(temp, "r");
+		fscanf(fp, "%s", bcBuf);
+		sprintf(sendBuf, "%016u%s", ID, bcBuf);
+		dataStorage[ID] = string(sendBuf);
+		// It's doesn't matter if send is failed
+		sendto(request->sock_fd,sendBuf,strlen(sendBuf),0,(struct sockaddr *)&(request->addr_client), request->client_len);
+		break;
 	}
 	delete request;
 }
